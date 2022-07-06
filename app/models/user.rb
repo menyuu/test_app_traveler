9b2427464d84e0b576a97a899e3e528c2d148efd
menyuu <101458830+menyuu@users.noreply.github.com>
2022-07-06 18:16:37 +0000

    フォローフォロワー機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   attachment :profile_image
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
+
+  def follow(user)
+    follower.create(followed_id: user) unless self == user
+  end
+
+  def unfollow(user)
+    follower.find_by(followed_id: user).destroy unless self == user
+  end
+
+  def following?(user)
+    following_user.include?(user)
+  end
 end
